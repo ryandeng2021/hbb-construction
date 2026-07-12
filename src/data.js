dd5ee8c29f0802d7e6799f457d793f3c67777b17
suzycheung-dot <Joilsuzy@163.com>
2026-07-12 16:50:06 -0700

    new changes

--- src/data.js
+++ src/data.js
@@ -353,6 +353,194 @@ export const whyChoose = [
 // Video section: { layout: 'one', video: { src: '/video/project.mp4', alt: 'Project Video' }, description: 'Optional text' }
 export const projects = [
   {
+    id: 17,
+    title: 'Single Family Home Addition & Remodel',
+    slug: 'project-17',
+    category: 'House Addition & Remodel',
+    status: 'completed',
+    image: getAssetPath('/img/17-3.jpg'),
+    description: 'Large openings allow daylight to penetrate deep into the interior, enhancing spatial depth and clarity.',
+    location: 'Sunnyvale',
+    year: '2026',
+    heroImage: getAssetPath('/img/17-1.jpg'),
+    content: [
+      {
+        layout: 'two',
+        images: [
+          {
+            src: getAssetPath('/img/17-2.jpg'),
+            alt: 'Single Family Home Remodel overview'
+          },
+          {
+            src: getAssetPath('/img/17-3.jpg'),
+            alt: 'Single Family Home Remodel overview'
+          }
+        ],
+        description: 'This project stands out with its expansive land area and spacious layout, allowing for a simple design approach witout elaborate decorations. The overall aesthetic is serene and understated, reflecting a tranquil beauty. The client is highly satisfied with the overall construction, as it perfectly captures the original design concept of the house.'
+      },
+      {
+        layout: 'two',
+        images: [
+          {
+            src: getAssetPath('/img/17-4.jpg'),
+            alt: 'Single Family Home Remodel overview'
+          },
+          {
+            src: getAssetPath('/img/17-5.jpg'),
+            alt: 'Single Family Home Remodel overview'
+          },
+        ],
+        description: 'This project stands out with its expansive land area and spacious layout, allowing for a simple design approach witout elaborate decorations. The overall aesthetic is serene and understated, reflecting a tranquil beauty. The client is highly satisfied with the overall construction, as it perfectly captures the original design concept of the house.'
+      },
+      {
+        layout: 'two',
+        images: [
+          {
+            src: getAssetPath('/img/17-6.jpg'),
+            alt: 'Residential units',
+          },
+          {
+            src: getAssetPath('/img/17-7.jpg'),
+            alt: 'Commercial office spaces',
+          },
+        ]
+      },
+      {
+        layout: 'two',
+        images: [
+          {
+            src: getAssetPath('/img/17-8.jpg'),
+            alt: 'Residential units',
+          },
+          {
+            src: getAssetPath('/img/17-9.jpeg'),
+            alt: 'Commercial office spaces',
+          },
+        ]
+      },
+      {
+        layout: 'two',
+        images: [
+          {
+            src: getAssetPath('/img/17-10.jpeg'),
+            alt: 'Residential units',
+          },
+          {
+            src: getAssetPath('/img/17-11.jpg'),
+            alt: 'Commercial office spaces',
+          },
+        ]
+      },
+      {
+        layout: 'two',
+        images: [
+          {
+            src: getAssetPath('/img/17-15.jpg'),
+            alt: 'Residential units',
+          },
+          {
+            src: getAssetPath('/img/17-16.jpg'),
+            alt: 'Commercial office spaces',
+          },
+        ]
+      },
+    ]
+  },
+  {
+    id: 18,
+    title: 'Single Family Home Addition & Remodel',
+    slug: 'project-18',
+    category: 'House Addition & Remodel',
+    status: 'completed',
+    image: getAssetPath('/img/18-3.jpg'),
+    description: 'Large openings allow daylight to penetrate deep into the interior, enhancing spatial depth and clarity.',
+    location: 'Sunnyvale',
+    year: '2026',
+    heroImage: getAssetPath('/img/18-1.jpg'),
+    content: [
+      {
+        layout: 'two',
+        images: [
+          {
+            src: getAssetPath('/img/18-2.jpg'),
+            alt: 'Single Family Home Remodel overview'
+          },
+          {
+            src: getAssetPath('/img/18-3.jpg'),
+            alt: 'Single Family Home Remodel overview'
+          }
+        ],
+        description: 'This project stands out with its expansive land area and spacious layout, allowing for a simple design approach witout elaborate decorations. The overall aesthetic is serene and understated, reflecting a tranquil beauty. The client is highly satisfied with the overall construction, as it perfectly captures the original design concept of the house.'
+      },
+      {
+        layout: 'two',
+        images: [
+          {
+            src: getAssetPath('/img/18-4.jpg'),
+            alt: 'Single Family Home Remodel overview'
+          },
+          {
+            src: getAssetPath('/img/18-5.jpg'),
+            alt: 'Single Family Home Remodel overview'
+          },
+        ],
+        description: 'This project stands out with its expansive land area and spacious layout, allowing for a simple design approach witout elaborate decorations. The overall aesthetic is serene and understated, reflecting a tranquil beauty. The client is highly satisfied with the overall construction, as it perfectly captures the original design concept of the house.'
+      },
+      {
+        layout: 'two',
+        images: [
+          {
+            src: getAssetPath('/img/18-6.jpg'),
+            alt: 'Residential units',
+          },
+          {
+            src: getAssetPath('/img/18-7.jpg'),
+            alt: 'Commercial office spaces',
+          },
+        ]
+      },
+      {
+        layout: 'two',
+        images: [
+          {
+            src: getAssetPath('/img/18-8.jpg'),
+            alt: 'Residential units',
+          },
+          {
+            src: getAssetPath('/img/18-9.jpeg'),
+            alt: 'Commercial office spaces',
+          },
+        ]
+      },
+      {
+        layout: 'two',
+        images: [
+          {
+            src: getAssetPath('/img/18-10.jpeg'),
+            alt: 'Residential units',
+          },
+          {
+            src: getAssetPath('/img/18-11.jpg'),
+            alt: 'Commercial office spaces',
+          },
+        ]
+      },
+      {
+        layout: 'two',
+        images: [
+          {
+            src: getAssetPath('/img/18-15.jpg'),
+            alt: 'Residential units',
+          },
+          {
+            src: getAssetPath('/img/18-16.jpg'),
+            alt: 'Commercial office spaces',
+          },
+        ]
+      },
+    ]
+  },
+  {
     id: 16,
     title: 'Single Family Home Addition & Remodel',
     slug: 'project-16',
@@ -565,10 +753,10 @@ export const projects = [
             alt: 'Residential units',
           },
           {
-              src: getAssetPath('/img/16-14.jpg'),
-              alt: 'Commercial office spaces',
+            src: getAssetPath('/img/16-14.jpg'),
+            alt: 'Commercial office spaces',
           },
-          ]
+        ]
       },
     ]
   },
